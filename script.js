694456388ad4a2b1e2d7edfd5fa466c8322b79b7
Ouioui456 <evenements.areches@gmail.com>
2026-02-12 11:55:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+// 1. On va chercher le fichier
+fetch('weather.json')
+    .then(reponse => reponse.json()) // 2. On transforme le texte en objet exploitable
+    .then(donnees => {
+        // 3. On sélectionne notre balise HTML
+        const zoneAffichage = document.getElementById('affichage-meteo');
+        
+        // 4. On écrit dedans (exemple si ton JSON contient une ville)
+        zoneAffichage.innerHTML = `La météo à ${donnees.ville} est de ${donnees.temp}°C`;
+    })
+    .catch(erreur => console.error("Le JSON n'a pas pu être chargé :", erreur));
