59bc9e75b9347fc96be150251ed0124669f7d0b5
Ouioui456 <evenements.areches@gmail.com>
2026-02-12 11:59:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const url = "https://api.open-meteo.com/v1/forecast?latitude=48.85&longitude=2.35&current_weather=true";
 // 1. On va chercher le fichier
 fetch('weather.json')
     .then(reponse => reponse.json()) // 2. On transforme le texte en objet exploitable
