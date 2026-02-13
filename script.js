4db1846fc11593db5f2e1dc5ab66fd9b1db6e04b
Ouioui456 <evenements.areches@gmail.com>
2026-02-13 11:36:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+//Ajout des éléments créateur du HTML 
+const.creatorView = document.getElementById('creator-view');
+const.quizzEdition = document.getElementById('quizz-edition');
+const.submit = document.getElementById('submit');
+const.questionCounter = document.getElementById('questioncounter'); 
+const.launcher = document.getElementById('launcher'); 
+//stockage des questions
+let questionsTableau = []; 
+//Ecoute de l'enregistrement des questions
