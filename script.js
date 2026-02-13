240e7d74f9f18035a2acad7e29629f7bd8008fd5
Ouioui456 <evenements.areches@gmail.com>
2026-02-13 12:06:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //Ajout des éléments créateur du HTML 
-const.creatorView = document.getElementById('creatorview');
-const.quizzEdition = document.getElementById('quizz-edition');
-const.submit = document.getElementById('submit');
-const.questionCounter = document.getElementById('questioncounter'); 
-const.launcher = document.getElementById('launcher');
+const creatorView = document.getElementById('creatorview');
+const quizzEdition = document.getElementById('quizz-edition');
+const submit = document.getElementById('submit');
+const questionCounter = document.getElementById('questioncounter'); 
+const launcher = document.getElementById('launcher');
 //stockage des questions
 let questionsTableau = []; 
 //Ecoute de l'enregistrement des questions
 quizzEdition.addEventListener('submit', function(event) {
   event.preventDefault(); 
   console.log("all good"); 
-  const.qinput = document.getElementById('question-input').value;
+  const qinput = document.getElementById('question-input').value;
   console.log("question enregistrée" + qinput);
 });
