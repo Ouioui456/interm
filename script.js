9694250bae6a194a6200919094e230ee4a27414e
Ouioui456 <evenements.areches@gmail.com>
2026-02-13 14:29:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,23 @@ const launcher = document.getElementById('launcher');
 let questionsTableau = []; 
 //Ecoute de l'enregistrement des questions
 quizzEdition.addEventListener('submit', function(event) {
+  //récupération des valeurs
+  const quest = document.getElementById('question-input').value;
+    const r1 = document.getElementById('ans1').value;
+    const r2 = document.getElementById('ans2').value;
+    const r3 = document.getElementById('ans3').value;
+    const r4 = document.getElementById('ans4').value;
+  const nouvelleQuestion = {
+    titre : quest; 
+    choix: [r1,r2,r3,r4],
+    bonnereponse : 0
+}; 
+EditionTableau.push(nouvelleQuestion); 
+quizzEdition.reset; 
+console.log("Tableau mis à jour", questionsTableau); 
   event.preventDefault(); 
   console.log("all good"); 
   const qinput = document.getElementById('question-input').value;
   console.log("question enregistrée" + qinput);
+  document.getElementById('question-input').value = "";
 });
